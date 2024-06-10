dbfa959ec1ddbca94d61bbda169a377a2fadc31c
Josep Manel Andres Moscardo <josep.moscardo@embl.de>
2024-06-10 12:31:28 +0200

    parallel begining

--- reindex.py
+++ reindex.py
@@ -2,6 +2,7 @@ import opensearchpy
 import urllib3
 import time
 import os
+
 from opensearchpy import OpenSearch, RequestsHttpConnection
 
 ES_USER = os.getenv('ES_USER')
@@ -12,6 +13,7 @@ es = OpenSearch(hosts=[{'host': 'opensearch-graylog.ktest.embl.de', 'port': 443}
                    use_ssl= 'true',
                    connection_class=RequestsHttpConnection,
                    verify_certs=False)
+
 
 # Function to reindex an index
 def reindex_index(es, source_index, target_index):
@@ -35,36 +37,51 @@ def create_index_settings(es, index_name, primary_shards, replica_shards):
     }
     es.indices.create(index=index_name, body=body, request_timeout=1000000000)
 
+def full_reindex(index):
+    count = es.count(index=batch_indices)['count']
+    print(f"Document count in index {batch_indices}: {count}")
+    
+    temp_index = f"temp_{index}"
+    print("Temp_index: ", temp_index)
+    time.sleep(60)
+    # Update settings for the temporary index
+    create_index_settings(es, temp_index, primary_shards=4, replica_shards=0)
+
+    # Reindex to temporary index
+    reindex_index(es, index, temp_index)
+
+    # Delete original index
+    es.indices.delete(index=index, request_timeout=1000000000)
+    # Update settings for the temporary index
+    create_index_settings(es, index, primary_shards=4, replica_shards=0)
+    # Reindex back to original index
+    reindex_index(es, temp_index, index)
+
+    # Delete the temporary index
+    es.indices.delete(index=temp_index, request_timeout=1000000000)
+
+    print(f"Reindexed {index} to {temp_index} and back to {index} with new settings.")
+    count = es.count(index=index)['count']
+
+    print(f"Document count in index {index}: {count}")
+
+
+
 # Loop over indices in batches
 start_index = 333
-end_index = 334
-batch_size = 1
+end_index = 340
+batch_size = 3
 
 for i in range(start_index, end_index + 1, batch_size):
     batch_indices = [f"graylog_{j}" for j in range(i, min(i + batch_size, end_index + 1))]
     print("Batch indices: ", batch_indices) 
 
+
     for index in batch_indices:
-        temp_index = f"temp_{index}"
-        print("Temp_index: ", temp_index)
+        print(index)
+        #time.sleep(60)
+        #full_reindex(index)
 
-        # Update settings for the temporary index
-        create_index_settings(es, temp_index, primary_shards=4, replica_shards=0)
-
-        # Reindex to temporary index
-        reindex_index(es, index, temp_index)
-
-	# Delete original index
-        es.indices.delete(index=index, request_timeout=1000000000)
-        # Update settings for the temporary index
-        create_index_settings(es, index, primary_shards=4, replica_shards=0)
-        # Reindex back to original index
-        reindex_index(es, temp_index, index)
-
-        # Delete the temporary index
-        es.indices.delete(index=temp_index, request_timeout=1000000000)
-
-        print(f"Reindexed {index} to {temp_index} and back to {index} with new settings.")
 
 print("Reindexing completed.")
 
